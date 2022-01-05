fee1110eb92ee47467b7819e6255c9c82319ee59
João Victor S. De Moraes <joao337.jvs@gmail.com>
2022-01-04 22:48:50 -0400

    Second Commit.

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,5 +9,43 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro1 = new Livro("Zendaya",300);
+        System.out.println(livro1);
+
     }
 }
+class Livro {
+
+    private String nome;
+    private Integer paginas;
+
+    public Livro(String nome, Integer paginas) {
+        this.nome = nome;
+        this.paginas = paginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(Integer paginas) {
+        this.paginas = paginas;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", paginas=" + paginas +
+                '}';
+    }
+}
+
